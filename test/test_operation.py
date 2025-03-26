2811d82d3cf05426a8d2ed88b2675adbe4af87be
Wassim Sliti <wassimsliti1998@gmail.com>
2025-03-26 22:59:25 +0100

    Unit test case updated

---
+++ test/test_operation.py
@@ -0,0 +1,14 @@
+from src.math_operations import add , sub
+
+def test_add():
+    assert add(2,3) == 5
+    assert add(-1,3) == 2
+    assert add(-1,1) == 0
+
+
+
+def test_sub():
+    assert sub(5,3) == 2
+    assert sub(10,5) == 5
+    assert sub(8,9) == -1
+    assert sub(8,8) == 0
